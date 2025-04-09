a19e83f51f78377bd8f8fdc28329c9a0f4bc932a
Dante <dante@devdante.com>
2025-04-08 21:49:52 -0600

    Change color style to default dark mode

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,16 +1,25 @@
 /* Copyright (c) 2024 Dante Passalacqua */
 
 import React from 'react';
+import { ReactComponent as SunIcon } from '../Images/sun-solid.svg';
 
 function Hero() {
 	return (
-		<div className="hero">
-			<img
-				src={require('../Images/world-icon.png')}
-				alt=""
-				className="world-icon"
-			/>
-			<p className="hero--title">dante's travel journal.</p>
+		<div className="hero-container">
+			<div>
+				<span></span>
+			</div>
+			<div className="hero">
+				<img
+					src={require('../Images/world-icon.png')}
+					alt=""
+					className="world-icon"
+				/>
+				<p className="hero--title">Travel Journal</p>
+			</div>
+			<div className="dark-mode-toggle">
+				<SunIcon className="dark-mode-icon" />
+			</div>
 		</div>
 	);
 }
